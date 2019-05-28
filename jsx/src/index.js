f33a5ba6a2c746c975f0ac16d487c14b0d436cdf
MonicaBarutia <monica.barutia@freshbyteinc.com>
2019-05-28 15:19:16 +0300

    Reference js variables in jsx.

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -2,13 +2,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+function getButtonText(){
+    return 'Click on me!';
+}
+
 // Create a react component
 const App = () => {
+    const buttonText = 'Click Me!';
+    const buttonText2 = {text: 'Click me'};
+    const style = {backgroundColor: 'blue', color: 'white'};
+    const labelText = 'Enter name:';
+
     return (
         <div>
             <label className="label" htmlFor="name">Enter name:</label>
+            <label className="label" htmlFor="name">{labelText}</label>
             <input id="name" type="text"/>
-            <button style={{backgroundColor: 'blue', color: 'white'}}>Submit</button>
+            <button style={{backgroundColor: 'blue', color: 'white'}}>{getButtonText()}</button>
+            <button style={{backgroundColor: 'blue', color: 'white'}}>{buttonText}</button>
+            <button style={style}>{buttonText2.text}</button>
+            {/*/!*<button style={{backgroundColor: 'blue', color: 'white'}}>{buttonText2}</button>*!/ nu merge asa - we can't call a js object as text*/}
         </div>
     );
 };
