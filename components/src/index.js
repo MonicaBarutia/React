bbab1f790fc906e4a90836768363a653d88d0feb
MonicaBarutia <monica.barutia@freshbyteinc.com>
2019-05-29 12:28:02 +0300

    Pass properties to components.

--- components/src/index.js
+++ components/src/index.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import faker from  'faker';
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail/>
+            <CommentDetail author="Sam" timeAgo="Today at 4:45PM" comment="Nice blog post!"
+                           image={faker.image.avatar()}/>
+            <CommentDetail author="Alex" timeAgo="Today at 2:00AM" comment="Very interesting."
+                           image={faker.image.avatar()}/>
+            <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" comment="Waiting for more updates."
+                           image={faker.image.avatar()}/>
         </div>
     );
 };
