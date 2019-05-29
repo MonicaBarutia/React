333877c68a73a71b62e2bcba46e77d35bb7b229f
MonicaBarutia <monica.barutia@freshbyteinc.com>
2019-05-29 13:38:42 +0300

    Show custom children (a component inside another component).

--- components/src/index.js
+++ components/src/index.js
@@ -2,16 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam" timeAgo="Today at 4:45PM" comment="Nice blog post!"
-                           image={faker.image.avatar()}/>
-            <CommentDetail author="Alex" timeAgo="Today at 2:00AM" comment="Very interesting."
-                           image={faker.image.avatar()}/>
-            <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" comment="Waiting for more updates."
-                           image={faker.image.avatar()}/>
+            <ApprovalCard>
+                <CommentDetail author="Sam" timeAgo="Today at 4:45PM" comment="Nice blog post!"
+                               image={faker.image.avatar()}/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Alex" timeAgo="Today at 2:00AM" comment="Very interesting."
+                               image={faker.image.avatar()}/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail author="Jane" timeAgo="Yesterday at 5:00PM" comment="Waiting for more updates."
+                               image={faker.image.avatar()}/>
+            </ApprovalCard>
         </div>
     );
 };
