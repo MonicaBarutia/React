2ca093b38fbb7bd94175c3422c3e9dadacb516cd
MonicaBarutia <monica.barutia@freshbyteinc.com>
2019-05-31 12:21:42 +0300

    Add request to api.

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -3,8 +3,13 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-    onSearchSubmit(term){
-        console.log(term);
+    onSearchSubmit(term) {
+        axios.get('https://api.unsplash.com/search/photos', {
+            params: { query: term },
+            headers: {
+                Authorization: 'Client-ID a246712260dc7830f5679b5f9c090ffa472324646c23f0f53d717844e42b3e73'
+            }
+        });
     }
 
     render() {
