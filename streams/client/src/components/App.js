6dc79984c044576159c758d1b6d95082b9051a58
MonicaBarutia <monica.barutia@freshbyteinc.com>
2019-06-05 17:00:59 +0300

    Create routes.

--- streams/client/src/components/App.js
+++ streams/client/src/components/App.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
 
 const App = () => {
-    return<div>App</div>;
+    return (
+        <div>
+            <BrowserRouter>
+                <div>
+                    <Route path="/" exact component={StreamList}/>
+                    <Route path="/streams/new" exact component={StreamCreate}/>
+                    <Route path="/streams/edit" exact component={StreamEdit}/>
+                    <Route path="/streams/show" exact component={StreamShow}/>
+                    <Route path="/streams/delete" exact component={StreamDelete}/>
+                </div>
+            </BrowserRouter>
+        </div>
+    );
 };
 
 export default App;
