e3b3851aa1a88a4c5b73d05c5cb3204af34874a6
MonicaBarutia <monica.barutia@freshbyteinc.com>
2019-06-05 14:15:28 +0300

    Fetch posts and users with unique user ids.

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,6 +1,13 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+    await dispatch(fetchPosts());
+
+    const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    userIds.forEach(id => dispatch(fetchUser(id)));
+};
+
 // we are creating a function that is returning a function
 export const fetchPosts = () => async dispatch =>{
         const response = await jsonPlaceholder.get('/posts');
@@ -8,10 +15,9 @@ export const fetchPosts = () => async dispatch =>{
         dispatch({ type:  'FETCH_POSTS', payload: response.data})
 };
 
-export const fetchUser = (id) => dispatch => _fetchUser(id, dispatch);
+export const fetchUser = (id) => async  dispatch => {
 
-const _fetchUser = _.memoize(async (id, dispatch) => {
     const response = await jsonPlaceholder.get(`/users/${id}`) ;
 
     dispatch({ type:  'FETCH_USER', payload: response.data})
-});
+};
