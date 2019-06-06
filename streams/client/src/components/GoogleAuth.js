4240d68b3c63cd9d5673b463861f76602ea24e26
MonicaBarutia <monica.barutia@freshbyteinc.com>
2019-06-06 10:56:30 +0300

    Wire up the google api library.

---
+++ streams/client/src/components/GoogleAuth.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+class GoogleAuth extends React.Component{
+    componentDidMount() {
+        window.gapi.load('client:auth2', () => {
+            window.gapi.client.init({
+                clientId: '578955737577-qt8i43pvk7p59rui60dmeh7ncr9uk0ng.apps.googleusercontent.com',
+                scope: 'email'
+            });
+        });
+    }
+
+    render() {
+        return <div>Google Auth</div>;
+    }
+}
+
+export default GoogleAuth;
