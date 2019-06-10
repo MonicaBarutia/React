6654a18dd1f57831bbc9f0ae7712844491801e28
MonicaBarutia <monica.barutia@freshbyteinc.com>
2019-06-10 10:13:12 +0300

    Select records from state.

--- streams/client/src/components/streams/StreamEdit.js
+++ streams/client/src/components/streams/StreamEdit.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-const StreamEdit = () => {
+const StreamEdit = (props) =>{
     return <div>StreamEdit</div>;
 };
-export default StreamEdit;
+
+const mapsStateToProps = (state, ownProps) => {
+  return { stream: state.streams[ownProps.match.params.id] };
+};
+
+export default connect(mapsStateToProps)(StreamEdit);
