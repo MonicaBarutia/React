e47f52a79f8a9a21658bc47536fd14d8ce1a7cc1
MonicaBarutia <monica.barutia@freshbyteinc.com>
2019-06-12 12:19:17 +0300

    Add ComponentDidUpdate.

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -4,9 +4,16 @@ import axios from 'axios';
 class ResourceList extends React.Component {
     state = {resources: []};
     async componentDidMount() {
-        const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`)
+        const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
 
         this.setState({resources: response.data});
+    }
+
+    async componentDidUpdate(prevProps) {
+        if ( prevProps.resource !== this.props.resource ) {
+            const response = await axios.get(`https://jsonplaceholder.typicode.com/${this.props.resource}`);
+            this.setState({resources: response.data});
+        }
     }
 
     render() {
